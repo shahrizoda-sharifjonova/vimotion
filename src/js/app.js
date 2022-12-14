9b828d23941fc65971ff5564fe5e9471e28fea94
shahrizoda <shahrizodadeveloping@gmail.com>
2022-12-14 23:50:08 +0500

    initial

--- src/js/app.js
+++ src/js/app.js
@@ -5,3 +5,23 @@ functions.isWebp();
 // import Swiper, { Navigation, Pagination } from 'swiper';
 
 // const swiper = new Swiper();
+
+const madeBtn = document.querySelectorAll('.made__button');
+const madeWrapper = document.querySelectorAll('.made__wrapper');
+
+madeBtn.forEach(btn=>{
+    btn.addEventListener('click', (e)=>{
+        madeWrapper.forEach(wrapper=>{
+            wrapper.classList.remove('active')
+        })
+        madeBtn.forEach(button=>{
+            button.classList.remove('active')
+        })
+        const target = btn.getAttribute("data-target")
+        btn.classList.add('active')
+        madeWrapper.forEach(maderapper=>{
+            const answer = document.querySelector(`#${target}`)
+            answer.classList.add('active')
+        })
+    })
+})
